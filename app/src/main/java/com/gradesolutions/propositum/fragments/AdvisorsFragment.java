70b8907a029b01ab0cb140d5d1af335e7751fe93
William <0920076@utp.edu.pe>
2017-07-14 01:46:11 -0500

    completando clases

--- app/src/main/java/com/gradesolutions/propositum/fragments/AdvisorsFragment.java
+++ app/src/main/java/com/gradesolutions/propositum/fragments/AdvisorsFragment.java
@@ -3,17 +3,27 @@ package com.gradesolutions.propositum.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.gradesolutions.propositum.PropoApp;
 import com.gradesolutions.propositum.R;
+import com.gradesolutions.propositum.adapters.AdvisorsAdapter;
+import com.gradesolutions.propositum.models.Advisor;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AdvisorsFragment extends Fragment {
-
+    private RecyclerView advisorsRecyclerView;
+    private AdvisorsAdapter advisorsAdapter;
+    private RecyclerView.LayoutManager advisorsLayoutManager;
+    private List<Advisor> advisors;
 
     public AdvisorsFragment() {
         // Required empty public constructor
@@ -24,7 +34,15 @@ public class AdvisorsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_advisors, container, false);
+        View view = inflater.inflate(R.layout.fragment_advisors, container, false);
+        advisorsRecyclerView = (RecyclerView) view.findViewById(R.id.advisorsRecyclerView);
+        advisors = PropoApp.getInstance().getAdvisors();
+        advisorsAdapter = (new AdvisorsAdapter()).setAdvisors(advisors);
+        advisorsLayoutManager = new LinearLayoutManager(view.getContext());
+        //recyclerview no sabe q tenemos los daots..hay q decirle
+        advisorsRecyclerView.setAdapter(advisorsAdapter);
+        advisorsRecyclerView.setLayoutManager(advisorsLayoutManager);
+        return view;
     }
 
 }
