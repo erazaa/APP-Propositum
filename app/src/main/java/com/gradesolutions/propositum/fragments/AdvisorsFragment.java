13bd218bdaa5ec7ccf5160b6ec487891087d6bfa
Anita <1531561@utp.edu.pe>
2017-08-04 18:32:18 -0500

    create advisorsdetail design and logical

--- app/src/main/java/com/gradesolutions/propositum/fragments/AdvisorsFragment.java
+++ app/src/main/java/com/gradesolutions/propositum/fragments/AdvisorsFragment.java
@@ -10,7 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.gradesolutions.propositum.PropoApp;
+
 import com.gradesolutions.propositum.R;
 import com.gradesolutions.propositum.adapters.AdvisorsAdapter;
 import com.gradesolutions.propositum.models.Advisor;
