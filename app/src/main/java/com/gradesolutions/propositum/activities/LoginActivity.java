3c2668a680332e039ad88a8b595b26119d431a3f
Kevin <1521122@utp.edu.pe>
2017-08-05 17:36:09 -0500

    implemtando creacion de aviso para asesores

--- app/src/main/java/com/gradesolutions/propositum/activities/LoginActivity.java
+++ app/src/main/java/com/gradesolutions/propositum/activities/LoginActivity.java
@@ -4,15 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 
 import com.gradesolutions.propositum.R;
 
 public class LoginActivity extends AppCompatActivity {
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, ContainerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
     public void goCreateAccount(View view){
         Intent intent = new Intent(this, CreateAccountActivity.class);
--- app/src/main/java/com/gradesolutions/propositum/fragments/RequestFragment.java
+++ app/src/main/java/com/gradesolutions/propositum/fragments/RequestFragment.java
@@ -1,6 +1,7 @@
 package com.gradesolutions.propositum.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -8,13 +9,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.gradesolutions.propositum.R;
+import com.gradesolutions.propositum.activities.CreateAdvisoriesActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RequestFragment extends Fragment {
+
+    Button botoncito;
 
 
     public RequestFragment() {
@@ -28,6 +33,14 @@ public class RequestFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_request, container, false);
         showToolbar("Solicitud", false, view);
+        botoncito = (Button) view.findViewById(R.id.button5);
+        botoncito.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), CreateAdvisoriesActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 
